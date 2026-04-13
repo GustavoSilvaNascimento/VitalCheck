f0329a48c45eaea16bcf372527ea1a81df756082
GustavoSilvaNascimento <gust.14.gust@gmail.com>
2026-04-12 21:12:54 -0300

    alteração visual no login, pagina inicial e cadastro

--- VitalCheck/View/LoginView.xaml.cs
+++ VitalCheck/View/LoginView.xaml.cs
@@ -6,4 +6,14 @@ public partial class LoginView : ContentPage
 	{
 		InitializeComponent();
 	}
+	
+    private void OnLoginClicked(object sender, EventArgs e)
+    {
+        // Lógica de login aqui
+    }
+
+    private void OnCadastroClicked(object sender, EventArgs e)
+    {
+        
+    }
 }
