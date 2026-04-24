4d32d23dd659f0f381de61bc0a6966345966ae9b
GustavoSilvaNascimento <gust.14.gust@gmail.com>
2026-04-24 11:46:37 -0300

    .

--- VitalCheck/model/Response/UserToken.cs
+++ VitalCheck/model/Response/UserToken.cs
@@ -1,19 +1,22 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using VitalCheck.Model.DataBases;
 
 namespace VitalCheck.Model.Response
 {
-    public class UserToken
+    [Table("UserToken")]
+    public class UserToken : BaseSQLiteModel
     {
-        public int Id { get; set; }
-
+        
+        public int UserId { get; set; }
         public string? Nome { get; set; }
         public string? UserName { get; set; }
 
         public string? Email { get; set; } 
 
-        public string? Senha { get; set; } 
+        
 
         public string? Genero { get; set; } 
 
